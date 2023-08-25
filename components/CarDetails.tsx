d5c0d2738f9a86ba0df0e9f0779506c134bf25b8
Alex <amignagaray@gmail.com>
2023-08-24 20:30:34 -0500

    set up search functionality

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -3,6 +3,7 @@ import { CarProps } from "@/types";
 import React, { Fragment } from "react";
 import Image from "next/image";
 import { Dialog, Transition } from "@headlessui/react";
+import { generateCarImageUrl } from "@/utils";
 
 interface CarDetailsProps {
   isOpen: boolean;
@@ -56,40 +57,40 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                   <div className="flex-1 flex flex-col gap-3">
                     <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
                       <Image
-                        src="/hero.png"
+                        src={generateCarImageUrl(car)}
                         alt="Car Model"
                         fill
                         priority
-                        className="object-contain"
+                        className="object-contain mt-5"
                       />
                     </div>
 
                     <div className="flex gap-3">
                       <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                         <Image
-                          src="/hero.png"
+                          src={generateCarImageUrl(car, "29")}
                           alt="Car Model"
                           fill
                           priority
-                          className="object-contain"
+                          className="object-contain mt-1"
                         />
                       </div>
                       <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                         <Image
-                          src="/hero.png"
+                          src={generateCarImageUrl(car, "33")}
                           alt="Car Model"
                           fill
                           priority
-                          className="object-contain"
+                          className="object-contain mt-5"
                         />
                       </div>
                       <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                         <Image
-                          src="/hero.png"
+                          src={generateCarImageUrl(car, "13")}
                           alt="Car Model"
                           fill
                           priority
-                          className="object-contain"
+                          className="object-contain mt-2"
                         />
                       </div>
                     </div>
--- utils/index.ts
+++ utils/index.ts
@@ -1,11 +1,15 @@
-export async function fetchCars() {
+import { CarProps, FilterProps } from "@/types";
+
+export async function fetchCars(filters: FilterProps) {
+   const {manufacturer, year, limit, model, fuel } = filters;
+
    const headers = {
       'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
       'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
    };
 
    const response = await fetch(
-      'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+      `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
          headers: headers
       }
    )
@@ -29,3 +33,20 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
  
    return rentalRatePerDay.toFixed(0);
  };
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+   // key...
+   const url = new URL('https://cdn.imagin.studio/getImage');
+
+   const { make, year, model } = car;
+
+   url.searchParams.append('customer', 'hrjavascript-mastery');
+   url.searchParams.append('make', make);
+   url.searchParams.append('modelFamily', model.split(" ")[0]);
+   url.searchParams.append('zoomType', 'fullscreen');
+   url.searchParams.append('modelYear', `${year}`);
+   // url.searchParams.append('zoomLevel', zoomLevel);
+   url.searchParams.append('angle', `${angle}`);
+
+   return `${url}`;
+}
